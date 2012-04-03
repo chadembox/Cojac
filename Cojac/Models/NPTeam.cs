e5f0018f6d46d02dffbdac701d8b94d58da444de
JeauxCom <jeaux.com@gmail.com>
2012-04-03 00:08:28 -0500

    Added Data classes and Rules

---
+++ Cojac/Models/NPTeam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CojacModel.Models
+{
+    public class NPTeam
+    {
+        public string TeamID { get; set;  }     // Team identifier (TE1, TW2)
+        public string TeamName { get; set; }    // NBA team (Thunder, Trailblazers, etc.)
+        public Int32 TeamRank { get; set;  }    // 1-8
+    }
+}
