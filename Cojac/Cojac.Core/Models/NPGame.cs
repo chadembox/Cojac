174c5300fa90aed0efa35262003935af83690fce
JeauxCom <jeaux.com@gmail.com>
2012-04-03 00:17:09 -0500

    Added NBA Plan data classes

---
+++ Cojac/Cojac.Core/Models/NPGame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CojacModel.Models
+{
+    public class NPGame
+    {
+        public string   SeriesID { get; set; }      // Series identifer (FK: NPSeries)
+        public Int32    Game { get; set; }          // Game number (1-7)
+        public DateTime GameDateTime { get; set; }  // Game date / start time
+        public Int32    Team1Score { get; set; }    // Score of higher-ranked team
+        public Int32    Team2Score { get; set; }    // Score of lower-ranked team 
+        public bool     SeriesWon { get; set; }     // Was series won with this game
+    }
+}
---
+++ Cojac/Cojac.Core/Models/NPSeries.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CojacModel.Models
+{
+    public class NPSeries
+    {
+        public string   SeriesID { get; set; }          // E1, W2, (F)inal, (C)hampion, (D)ark Horse
+        public Int16    Round { get; set;  }            // 1, 2, 3, 4
+        public string   Team1 { get; set;  }            // Higher-ranked team
+        public Int16    Team1Wins { get; set; }         // Number of wins for higher-ranked team (FK: NPTeam)
+        public string   Team2 { get; set;  }            // Lower-ranked team
+        public string   Team2Wins { get; set; }         // Number of wins for lower-ranked team (FK: NPTeam)
+    }
+}
